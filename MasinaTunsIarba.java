deb8c1a5f91fba8d7140fe6226097fbf14f9b78a
giuliamarin1011 <marin.giulia1011@gmail.com>
2026-02-23 19:53:21 +0200

    Create MasinaTunsIarba.java

---
+++ MasinaTunsIarba.java
@@ -0,0 +1,182 @@
+package ProiectPOO;
+import java.io*;
+
+
+
+
+public class MasinaTunsIarba extends Aparat implements Serializable {
+    // datele membre
+   
+    private String marca;
+    private String model;
+    private double putere;
+    private double latimeTaiere;
+    private double capacitateRezervor;
+    private boolean rotiDeplasare;
+    private int nivelZgomot;
+    private double greutate;
+    private double garantie;
+    private String tipMotor;
+  
+    
+    //co fara arg
+     public MasinaTunsIarba() {
+        this.marca = "";
+        this.model = "";
+        this.putere = 0;
+        this.latimeTaiere = 0;
+        this.capacitateRezervor = 0;
+        this.rotiDeplasare = false;
+        this.nivelZgomot = 0;
+        this.greutate = 0;
+        this.garantie = 0;
+        this.tipMotor = "";
+    }
+     
+     
+     // co cu arg
+      public MasinaTunsIarba(String marca, String model, double putere, double latimeTaiere,double capacitateRezervor, boolean rotiDeplasare, int nivelZgomot,double greutate, double garantie, String tipMotor)
+      {
+        this.marca = marca;
+        this.model = model;
+        this.putere = putere;
+        this.latimeTaiere = latimeTaiere;
+        this.capacitateRezervor = capacitateRezervor;
+        this.rotiDeplasare = rotiDeplasare;
+        this.nivelZgomot = nivelZgomot;
+        this.greutate = greutate;
+        this.garantie = garantie;
+        this.tipMotor = tipMotor;
+    }
+        //co de copiere- copiere manuala     
+    public MasinaTunsIarba(MasinaTunsIarba masina) {
+        
+        
+    this.marca = masina.marca;
+    this.model = masina.model;
+    this.putere = masina.putere;
+    this.latimeTaiere = masina.latimeTaiere;
+    this.capacitateRezervor = masina.capacitateRezervor;
+    this.rotiDeplasare = masina.rotiDeplasare;
+    this.nivelZgomot = masina.nivelZgomot;
+    this.greutate = masina.greutate;
+    this.garantie = masina.garantie;
+    this.tipMotor = masina.tipMotor;
+ }  
+        //setteri si getteri
+    
+    //1
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+    //2
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+
+    //3
+    public double getPutere() { return putere; }
+    public void setPutere(double putere) { this.putere = putere; }
+    
+   //4
+    public double getLatimeTaiere() { return latimeTaiere; }
+    public void setLatimeTaiere(double latimeTaiere) { this.latimeTaiere = latimeTaiere; }
+
+    //5
+    public double getCapacitateRezervor() { return capacitateRezervor; }
+    public void setCapacitateRezervor(double capacitateRezervor) { this.capacitateRezervor = capacitateRezervor; }
+    
+    
+    //6
+    public boolean areRotiDeplasare() { return rotiDeplasare; }
+    public void setRotiDeplasare(boolean rotiDeplasare) { this.rotiDeplasare = rotiDeplasare; }
+
+    
+    //7
+    public int getNivelZgomot() { return nivelZgomot; }
+    public void setNivelZgomot(int nivelZgomot) { this.nivelZgomot = nivelZgomot; }
+
+    //8
+    public double getGreutate() { return greutate; }
+    public void setGreutate(double greutate) { this.greutate = greutate; }
+
+    
+    //9
+    public double getGarantie() { return garantie; }
+    public void setGarantie(double garantie) { this.garantie = garantie; }
+    
+    
+    //10
+    public String getTipMotor() { return tipMotor; }
+    public void setTipMotor(String tipMotor) { this.tipMotor = tipMotor; }
+
+       /*
+
+  public static void conditiiMasinaTunsIarba(ArrayList<MasinaTunsIarba> masini, double putereMinima, String marcaCautata){
+    for(int i=0;i<masini.size();i++){
+        MasinaTunsIarba m1= masini.get(i); // ca (v[i]) sau (masinatunsiarba)v[i]  -- extrag ob!!
+        if(m1.getPutere()>=putereMinima && m1.getMarca().equals(marcaCautata)){  //equalsIgnoreCase
+            System.out.println(m1); }
+    }
+ }
+// afisare crescatoare dupa putere a masinilor din marca bosh
+   public static void masiniBoshOrdCrescator(ArrayList<MasinaTunsIarba> masini,String marcaCeruta){
+        ArrayList<MasinaTunsIarba> filtrate = new ArrayList<>();
+    for (int i = 0; i < masini.size(); i++) {
+        MasinaTunsIarba m = masini.get(i);
+        if (m.getMarca().equals(marcaCeruta)) {   //
+            filtrate.add(m);
+        }
+    }
+    
+//  sort pt sortarea crescatoare
+   for(int i=0;i<filtrate.size()-1;i++){                                                                                                                                   // filtrate.sort((a, b) -> Double.compare(b.getPutere(), a.getPutere()));\
+      for(int j=i+1;j<filtrate.size();j++){
+          if(filtrate.get(i).getPutere()>filtrate.get(j).getPutere()){
+              MasinaTunsIarba aux= filtrate.get(i);
+              filtrate.set(i, filtrate.get(j));
+              filtrate.set(j, aux); 
+          }
+      } 
+   }
+                                                                                                                                                                                
+    System.out.println("\nMasini marca Bosh ordonate crescator dupa putere:");
+    for (int i = 0; i < filtrate.size(); i++) {
+        System.out.println(filtrate.get(i));
+    }
+   }
+   
+   
+
+    */
+    // met toString
+    @Override
+    public String toString() {
+        return "Masina de tuns iarba: " +
+                "marca=" + marca + '\'' +
+                ", model=" + model + '\'' +
+                ", putere=" + putere +
+                ", latime taiere=" + latimeTaiere +
+                ", capacitate rezervor=" + capacitateRezervor +
+                ", roti de deplasare=" + rotiDeplasare +
+                ", nivelZgomot=" + nivelZgomot +
+                ", greutate=" + greutate +
+                ", garantie=" + garantie +
+                ", tipMotor='" + tipMotor + '\'';
+              
+    }
+    
+    //met abstracte
+    @Override
+    public void schimba_starea(Rezultat a) {
+        System.out.println("Masina " + marca + " " + model + " a tuns " + a.produs + " mp de iarba.");
+        curent -= a.curent_consumat;  //cat curent a ramas ( scade din total)
+    }
+    
+    @Override
+    public void cost_mentenanta(Rezultat c) {
+        double cost = (c.functionare_ramasa < 10) ? 50 : 20;
+        System.out.println("Cost mentenanta pentru " + marca + " este " + cost + " lei.");  // calculeaza costul in functie de cat a fost folosit
+    }
+
+    
+          
+}
