86a27af0ee09b10577d39247a4dedaa262af6bac
giuliamarin1011 <marin.giulia1011@gmail.com>
2026-02-23 20:16:11 +0200

    Update Rezultat.java

--- Rezultat.java
+++ Rezultat.java
@@ -1,12 +1,30 @@
 package ProiectPOO;
 
 public class Rezultat {
-    protected double produs;            
-    protected double curent_consumat;   
+    protected double produs;
+    protected double curent_consumat;
+    protected double functionare_ramasa;
+
 
     public Rezultat(double produs, double curent_consumat) {
         this.produs = produs;
         this.curent_consumat = curent_consumat;
+        this.functionare_ramasa = 0; // default
+    }
+
+    public Rezultat(double produs, double curent_consumat, double functionare_ramasa) {
+        this.produs = produs;
+        this.curent_consumat = curent_consumat;
+        this.functionare_ramasa = functionare_ramasa;
+    }
+
+    //  getter/setter
+    public double getFunctionare_ramasa() {
+        return functionare_ramasa;
+    }
+
+    public void setFunctionare_ramasa(double functionare_ramasa) {
+        this.functionare_ramasa = functionare_ramasa;
     }
 
     @Override
@@ -14,6 +32,7 @@ public class Rezultat {
         return "Rezultat{" +
                 "produs=" + produs +
                 ", curent_consumat=" + curent_consumat +
+                ", functionare_ramasa=" + functionare_ramasa +
                 '}';
     }
 }
